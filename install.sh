36d996674f6ecc2b8825caff8d06927ab60175a9
Giles Westwood <Giles.Westwood@semantico.com>
2016-10-08 17:33:02 +0100

    correctly set fqdn and test directory creation to allow re-runs

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ fatal(){
 
 [ -z $1 ] && fatal "you must provide the public hostname of your edgerouter"
 
+fqdn=$1
+
 # Creating persistant letsencrypt directories and downloading files
-mkdir /config/letsencrypt/
+[ -d /config/letsencrypt ] || mkdir /config/letsencrypt/
 curl -o /config/letsencrypt/acme_tiny.py https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py
 chmod 755 /config/letsencrypt/acme_tiny.py
 curl -o /config/letsencrypt/letsrenew.sh https://raw.githubusercontent.com/rholmboe/ubnt-letsencrypt/master/letsrenew.sh
@@ -23,14 +25,14 @@ openssl genrsa 4096 | tee /config/letsencrypt/domain.key
 openssl req -new -sha256 -key /config/letsencrypt/domain.key -subj "/CN=$fqdn" | tee /config/letsencrypt/domain.csr
 
 # Making lighttpd configurations and restarting daemon
-mkdir /config/lighttpd/
+[ -d /config/lighttpd ] ||  mkdir /config/lighttpd/
 curl -o /config/lighttpd/lighttpd.conf https://raw.githubusercontent.com/rholmboe/ubnt-letsencrypt/master/lighttpd.conf
 ln -sf /config/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf
 ps -e | grep lighttpd | awk '{print $1;}' | xargs kill
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
 
 # Create acme respons directory
-mkdir -p /var/www/htdocs/.well-known/acme-challenge/
+[ -d /var/www/htdocs/.well-known/acme-challenge/ ] || mkdir -p /var/www/htdocs/.well-known/acme-challenge/
 
 # Run letsrenew.sh file for initial connect and/or renewal, doesn't matter
 bash /config/letsencrypt/letsrenew.sh
